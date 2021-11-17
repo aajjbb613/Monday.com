6a7bb890480b7db97f9208cb9af7ce53a2711257
Anthony Bradt <94542755+aajjbb613@users.noreply.github.com>
2021-11-17 09:33:06 -0500

    Create Basic_Read.py

---
+++ Basic_Read.py
@@ -0,0 +1,24 @@
+#TO READ FROM MONDAY
+
+import requests
+import json
+
+apiKey = "XXX"
+apiUrl = "https://api.monday.com/v2"
+headers = {"Authorization" : apiKey}
+
+#query = 'query{ boards (ids: 695791534){items{name id column_values{title id value}}}}'
+#query = 'query{ boards (ids: 711201285){items{name id}}}'
+query = 'query{ boards (ids: 711201285){items(ids: 1392408871){name column_values{title id value text}}}}'
+#query = 'query{ boards (ids: 695791534){items{name column_values(ids:status1){value text}}}}'
+data = {'query' : query}
+
+#711201285 = Mattamy Homes  1392408871 
+#695791534 = Minto
+#718348165 = Caivan
+#1190887252 = Urbandale
+
+
+r = requests.post(url=apiUrl, json=data, headers=headers) # make request
+x = r.json()
+print(x)
